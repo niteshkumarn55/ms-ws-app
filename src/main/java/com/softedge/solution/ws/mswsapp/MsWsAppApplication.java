f35ae829d8580cfe5568013944f026eca0ae8a44
Nitesh Kumar N <niteshkumarn55@gmail.com>
2020-02-15 17:08:10 +0530

    First commit1

---
+++ src/main/java/com/softedge/solution/ws/mswsapp/MsWsAppApplication.java
@@ -0,0 +1,13 @@
+package com.softedge.solution.ws.mswsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsWsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsWsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softedge/solution/ws/mswsapp/MsWsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softedge.solution.ws.mswsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsWsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
